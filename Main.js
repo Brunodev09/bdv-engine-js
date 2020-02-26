5fbb06c9ddad9d8f2103240183ef7ddd697dcb37
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2020-02-25 21:20:41 -0300

    Adjusting todos.

--- Main.js
+++ Main.js
@@ -28,7 +28,7 @@ window.onload = function () {
     // test.conways(100, 100, "green", "lightgreen", 100);
 
     test.activateImageDataRendering();
-    test.newGameObject("RECTANGLE", 0, 0, 100, 100, "BLACK", null, null, [0, 255, 0, 0], 2);
+    test.newGameObject("RECTANGLE", 0, 0, 100, 100, "RED", null, null, [0, 0, 0, 1], 2);
     // test.render2.pixelDoodling();
 
     // let movingSquare = test.drawingVectors();
